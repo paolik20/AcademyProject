d02ae49ede1a7c8f4c4b7e309b9105ee82bd77e9
katy <katy@mbp-di-katya.home-life.hub>
2021-12-22 16:54:23 +0100

    Speriamo non si rovini tutto

--- src/main/java/control/LoginAdminServlet.java
+++ src/main/java/control/LoginAdminServlet.java
@@ -1,24 +1,24 @@
 package control;
 
 import java.io.IOException;
+/*
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;*/
 import model.Admin;
 import service.AdminService;
 import service.ServiceException;
 import service.impl.AdminServiceImpl;
 
-/*
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-*/
 
 /**
  * Servlet implementation class LoginAdmin
