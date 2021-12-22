4cb5e29f96f83f8c254ce38e0d22f3bf7dde6832
paoli <paoli@LAPTOP-UU9QFB6E.homenet.telecomitalia.it>
2021-12-22 10:27:55 +0100

    aggiunta admin e merge

---
+++ src/main/java/control/findAllAcademiesServlet.java
@@ -0,0 +1,51 @@
+package control;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Academy;
+import service.AcademyService;
+import service.ServiceException;
+import service.impl.AcademyServiceImpl;
+
+/**
+ * Servlet implementation class findAllAcademiesServlet
+ */
+@WebServlet("/findAllAcademiesServlet")
+public class findAllAcademiesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	AcademyService academyService = new AcademyServiceImpl();
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public findAllAcademiesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			HttpSession session = request.getSession();
+			// session.getAttribute("usernameAdmin");
+			
+			List<Academy> academies = academyService.findAll();
+			request.setAttribute("academies", academies);
+			request.getRequestDispatcher("academies.jsp").forward(request, response);
+		} catch (ServiceException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
